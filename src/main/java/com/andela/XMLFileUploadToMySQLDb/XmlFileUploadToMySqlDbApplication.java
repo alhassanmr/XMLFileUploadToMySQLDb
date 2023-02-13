d0b0ebe8ee0580a41206ee222cf3c08ae236b54e
Alhassan Moro <alhassanmr@gmail.com>
2023-02-13 11:19:06 +0000

    first commit of project creation

---
+++ src/main/java/com/andela/XMLFileUploadToMySQLDb/XmlFileUploadToMySqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.andela.XMLFileUploadToMySQLDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFileUploadToMySqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlFileUploadToMySqlDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andela/XMLFileUploadToMySQLDb/XmlFileUploadToMySqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andela.XMLFileUploadToMySQLDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlFileUploadToMySqlDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
