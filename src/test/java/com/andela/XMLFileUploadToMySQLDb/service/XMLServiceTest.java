1bc636fe90b98c1ef7198a3d780f8797f77573e3
Alhassan Moro <alhassanmr@gmail.com>
2023-02-13 14:16:35 +0000

    about to dockerize application

--- src/test/java/com/andela/XMLFileUploadToMySQLDb/service/XMLServiceTest.java
+++ src/test/java/com/andela/XMLFileUploadToMySQLDb/service/XMLServiceTest.java
@@ -6,22 +6,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.imageio.metadata.IIOMetadataNode;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -39,15 +45,6 @@ class XMLServiceTest {
     private XMLService xmlService;
     @Mock
     private XMLRepository xmlRepository;
-    @Mock
-    private Document document;
-    @Mock
-    private NodeList nodeList;
-    @Mock
-    private Node node;
-    @Mock
-    private Element element;
-
     int page = 0;
     int size = 10;
     String sortBy = "createdAt";
@@ -76,33 +73,46 @@ class XMLServiceTest {
 
     @Test
     void testParseXMLData() {
+        // Given
+        XMLService xmlService = mock(XMLService.class);
         Document document = mock(Document.class);
         NodeList nList = mock(NodeList.class);
         Node nNode = mock(Node.class);
         Element eElement = mock(Element.class);
-
         when(document.getElementsByTagName("deviceInfo")).thenReturn(nList);
         when(nList.getLength()).thenReturn(1);
         when(nList.item(0)).thenReturn(nNode);
         when(nNode.getNodeType()).thenReturn(Node.ELEMENT_NODE);
+        when(nNode.getNodeType()).thenReturn(Node.ELEMENT_NODE);
+        when(eElement.getElementsByTagName("newspaperName")).thenReturn(nList);
+        when(nList.item(0)).thenReturn(nNode);
+        when(nNode.getTextContent()).thenReturn("The Times");
+        when(eElement.getElementsByTagName("screenInfo")).thenReturn(nList);
+        when(nList.item(0)).thenReturn(nNode);
+        NamedNodeMap attributes = mock(NamedNodeMap.class);
+        when(nNode.getAttributes()).thenReturn(attributes);
+        Node widthAttribute = mock(Node.class);
+        when(attributes.getNamedItem("width")).thenReturn(widthAttribute);
+        when(widthAttribute.getNodeValue()).thenReturn("1920");
+        Node heightAttribute = mock(Node.class);
+        when(attributes.getNamedItem("height")).thenReturn(heightAttribute);
+        when(heightAttribute.getNodeValue()).thenReturn("1080");
+        Node dpiAttribute = mock(Node.class);
+        when(attributes.getNamedItem("dpi")).thenReturn(dpiAttribute);
+        when(dpiAttribute.getNodeValue()).thenReturn("72");
 
-        when(eElement.getElementsByTagName("newspaperName").item(0).getTextContent()).thenReturn("The Times");
-        when(eElement.getElementsByTagName("screenInfo").item(0).getAttributes().getNamedItem("width").getNodeValue()).thenReturn("800");
-        when(eElement.getElementsByTagName("screenInfo").item(0).getAttributes().getNamedItem("height").getNodeValue()).thenReturn("600");
-        when(eElement.getElementsByTagName("screenInfo").item(0).getAttributes().getNamedItem("dpi").getNodeValue()).thenReturn("96");
-
-        XMLRepository xmlRepository = mock(XMLRepository.class);
-
+        // When
         xmlService.parseXMLData(document, "test.xml");
 
+        // Then
         XMLData xmlData = new XMLData();
         xmlData.setNewspaperName("The Times");
-        xmlData.setScreenWidth("800");
-        xmlData.setScreenHeight("600");
-        xmlData.setScreenDpi("96");
+        xmlData.setScreenWidth("1920");
+        xmlData.setScreenHeight("1080");
+        xmlData.setScreenDpi("72");
         xmlData.setFilename("test.xml");
-
-        verify(xmlRepository).save(xmlData);
+        xmlData.setUploadTime(new Timestamp(System.currentTimeMillis()));
+        verify(xmlRepository, times(1)).save(xmlData);
     }
 
     @Test
@@ -140,15 +150,12 @@ class XMLServiceTest {
     }
 
 
-
-
     private Page<XMLData> createMockData() {
         List<XMLData> content = Arrays.asList(
                 new XMLData(),
                 new XMLData(),
                 new XMLData()
         );
-        PageImpl<XMLData> page = new PageImpl<>(content);
-        return page;
+        return new PageImpl<>(content);
     }
 }
