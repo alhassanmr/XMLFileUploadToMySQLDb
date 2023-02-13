d1fe5bc29b5aecb741daef40a0a6afc1deb975a7
Alhassan Moro <alhassanmr@gmail.com>
2023-02-13 11:30:56 +0000

    adding entity class

---
+++ src/main/java/com/andela/XMLFileUploadToMySQLDb/entity/XMLData.java
@@ -0,0 +1,45 @@
+package com.andela.XMLFileUploadToMySQLDb.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name = "xml_data")
+public class XMLData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "newspaper_name")
+    private String newspaperName;
+
+    @Column(name = "screen_width")
+    private String screenWidth;
+
+    @Column(name = "screen_height")
+    private String screenHeight;
+
+    @Column(name = "screen_dpi")
+    private String screenDpi;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "upload_time")
+    private Timestamp uploadTime;
+
+    @Column(name = "filename")
+    private String filename;
+
+}
