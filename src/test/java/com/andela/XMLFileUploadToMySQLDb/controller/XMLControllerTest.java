94bde9791d6424ab6dd183752fc97e299a5632d0
Alhassan Moro <alhassanmr@gmail.com>
2023-02-16 09:44:16 +0000

    added controller test cases

--- src/test/java/com/andela/XMLFileUploadToMySQLDb/controller/XMLControllerTest.java
+++ src/test/java/com/andela/XMLFileUploadToMySQLDb/controller/XMLControllerTest.java
@@ -12,10 +12,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 import org.w3c.dom.Document;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,43 +36,82 @@ class XMLControllerTest {
     @Mock
     private XMLService xmlService;
 
-    //testing a valid xml file
     @Test
-    public void testUploadXMLFile() throws Exception {
-//        MultipartFile file = mock(MultipartFile.class);
-//        when(file.isEmpty()).thenReturn(false);
-//        Document document = mock(Document.class);
-//        when(xmlService.isXMLValid(file)).thenReturn(document);
-//        ResponseEntity<String> response = xmlController.uploadXMLFile(file);
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals("File uploaded successfully", response.getBody());
-//        verify(xmlService).isXMLValid(file);
-//        verify(xmlService).parseXMLData(document, file.getOriginalFilename());
-    }
+    void testUploadXMLFileWithValidFile() throws Exception {
+        // create a valid XML file
+        String xml = "<root><data>test</data></root>";
+        MockMultipartFile file = new MockMultipartFile("file", "test.xml", "text/xml", xml.getBytes());
 
-    //testing empty file
-    @Test
-    public void testUploadXMLFileWithEmptyFile() {
-        MultipartFile file = mock(MultipartFile.class);
-        when(file.isEmpty()).thenReturn(true);
+        // call the uploadXMLFile method
         ResponseEntity<String> response = xmlController.uploadXMLFile(file);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("No file received", response.getBody());
-        verify(xmlService, never()).isXMLValid(file);
-        verify(xmlService, never()).parseXMLData(any(Document.class), anyString());
+
+        // assert that the response is successful and contains the expected message
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("File uploaded successfully", response.getBody());
     }
 
     @Test
-    public void testUploadXMLFileWithInvalidXML() {
-//        MultipartFile file = mock(MultipartFile.class);
-//        when(file.isEmpty()).thenReturn(false);
-//        when(xmlService.isXMLValid(file)).thenReturn(false);
-//        ResponseEntity<String> response = xmlController.uploadXMLFile(file);
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals("Invalid XML file", response.getBody());
-//        verify(xmlService).isXMLValid(file);
-//        verify(xmlService, never()).parseXMLData(any(Document.class), anyString());
+    void testUploadXMLFileWithInvalidFile() throws Exception {
+        // create an invalid XML file
+        String xml = "<root><data>test</data>";
+        MockMultipartFile file = new MockMultipartFile("file", "test.xml", "text/xml", xml.getBytes());
+
+        // call the uploadXMLFile method
+        ResponseEntity<String> response = xmlController.uploadXMLFile(file);
+
+        // assert that the response is a bad request and contains the expected error message
+        assertEquals(400, response.getStatusCodeValue());
+        assertEquals("Invalid XML file", response.getBody());
     }
 
+    @Test
+    void testUploadXMLFileWithNoFile() throws Exception {
+        // create an empty file
+        MockMultipartFile file = new MockMultipartFile("file", new byte[0]);
+
+        // call the uploadXMLFile method
+        ResponseEntity<String> response = xmlController.uploadXMLFile(file);
+
+        // assert that the response is a bad request and contains the expected error message
+        assertEquals(400, response.getStatusCodeValue());
+        assertEquals("No file received", response.getBody());
+    }
+
+    @Test
+    void testGetXMLDataWithFilter() {
+        // create a filter with some values
+        String newspaperName = "Test Newspaper";
+        String screenWidth = "1920";
+        String filename = "test.xml";
+        String sortField = "date";
+        String isDescending = "true";
+
+        // call the getXMLData method with the filter
+        ResponseEntity<List<XMLData>> response = xmlController.getXMLData(newspaperName, screenWidth, null, null, filename, sortField, isDescending, 0, 5);
+
+        // assert that the response is successful and contains some data
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(5, response.getBody().size());
+    }
+
+    @Test
+    void testGetXMLDataWithEmptyFilter() {
+        // call the getXMLData method with an empty filter
+        ResponseEntity<List<XMLData>> response = xmlController.getXMLData(null, null, null, null, null, null, null, 0, 5);
+
+        // assert that the response is successful and contains an empty list
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(Collections.emptyList(), response.getBody());
+    }
+
+    @Test
+    void testGetXMLDataWithPagination() {
+        // call the getXMLData method with pagination parameters
+        ResponseEntity<List<XMLData>> response = xmlController.getXMLData(null, null, null, null, null, null, null, 1, 5);
+
+        // assert that the response is successful and contains the correct number of items
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(5, response.getBody().size());
+    }
 
 }
